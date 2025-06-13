4b0f92f337fa543ff896186f6b341331f3e199d3
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-13 21:30:47 +0000

    Conversion changes

---
+++ 01_Basics/Typecast.js
@@ -0,0 +1,17 @@
+let score = "44"
+let valueInNumber = Number(score);
+console.log(typeof score);
+console.log(typeof valueInNumber);
+
+/*
+Number type cast value may come NaN -> Not a number 
+"33" -> 33
+"33av" -> NaN
+false -> 0
+undefined -> NaN
+null -> 0
+1   -> true 
+""  -> true 
+"ahaj" -> false
+*/
+
