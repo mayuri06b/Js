bc492075a25f22390010e592d135c4718428549c
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-13 11:58:00 +0000

    Datatypes added

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,11 @@
+/*
+number(2^53) ,bigint , string , boolean 
+null => standalone value represents empty 
+undefined -> not assigned 
+symbol => unique
+*/
+
+// Object 
+
+console.log(typeof null);   // object
+console.log(typeof undefined);      // undefined
---
+++ 01_Basics/Variables.js
@@ -0,0 +1,30 @@
+// Starting with Variables the simplest way of Storing the items values which will be used in our project 
+// Simple way of Storing the data 
+
+// const -> no changes in the values are allowed 
+// var -> is not used now in latest Java Script beacuse of the functional and Block scope Issue 
+// <variableName> <value>; -> No error 
+
+let adminPassword = "temp123";
+
+let adminName = "Mayuri Barapatre"
+
+let adminCity = "Nagpur"
+
+adminPhoneNo = 8902820222
+
+let noData ;
+
+console.table([adminName , adminPassword , adminPhoneNo , adminCity , noData]);
+/*
+Shows a beautiful way of printing in a table format 
+*/
+
+// no value -> by default gets value as undefined
+
+console.log("Mayuri");
+
+/* 2 documentatins for JS -> 
+    - mdnDoc 
+    - https://262.ecma-international.org/14.0/ (ecma)
+*/
