6ac7e3c2991a0530854b3812f033579cfcd05b67
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-14 09:59:45 +0000

    commited

--- 01_Basics/DataTypes.js
+++ 01_Basics/DataTypes.js
@@ -10,3 +10,27 @@ symbol => unique
 console.log(typeof null);   // object
 console.log(typeof undefined);      // undefined
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.table([id , anotherId]);
+console.log(id === anotherId);
+
+
+// Primitive Vs Non Primitive -> based on how data resides in memory and how it is Accessed 
+
+// Primitive 
+/* 7types : String , Number , Boolean , null , undefined , Symbol , BigInt (bigNumber)*/
+//Refernce type (Non Primitive)
+
+// Arrays , Objects , Functions 
+// [] , {key : value} 
+
+// JS dynamically typed Language 
+
+
+// Function Object 
+const myFunc = function () {
+    console.log("Hellow !!");
+}
+console.log(typeof myFunc);
