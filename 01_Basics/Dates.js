a96c0dd88b0fbe0ad22644fb5323cf60e873cc24
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-14 10:51:31 +0000

    commited

---
+++ 01_Basics/Dates.js
@@ -0,0 +1,32 @@
+// Dates 
+let mydate = new Date();
+// Object in javaScript
+
+
+//Month starts fm 0 in jaavscript
+// Sat Feb 24 2024
+let newDate = new Date(2024 , 1 , 24);
+console.log(newDate.toDateString());
+
+// console.log(mydate.getDate());
+
+let timeStamp = Date.now()
+
+// getTime()
+
+// convert to seconds
+
+// getMonth - starts with 0 
+
+console.log(Math.floor(timeStamp/1000));
+
+
+// This is mainly used for Customization purposes 
+
+let time1 = new Date();
+time1.toLocaleString('default' , {
+    weekday: "long",
+})
+
+console.log(time1);
+
---
+++ 02_Arrays/arrays1.js
@@ -0,0 +1,23 @@
+// arrays
+
+// can be of diffrent types  are resizeable 
+// Zero based index , shallow copy 
+
+
+// Shallow copy -> same refernce point 
+// deep copy -> different refernce point 
+
+// prototype -> prototype 
+
+let myArr = new Array();
+myArr.push(1);
+myArr.pop();
+
+// shift removes from start 
+// unshift pushes to the start 
+
+// slice -> section of array , splice ->  removes that section from the original array 
+// and returns including the last val 
+
+// join()
+// converts to string , sepearated 
