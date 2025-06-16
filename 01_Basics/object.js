a232817e9e949071f8f1163f3bd15610a6e1771f
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-16 18:41:19 +0000

    Commited Scope

--- 01_Basics/object.js
+++ 01_Basics/object.js
@@ -30,3 +30,51 @@ const tinderUser = {} // Non Singleton
 
 // Nested Object 
 
+// Object.assign()
+// when u want to merge two objects 
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target);
+// Expected output: true
+
+//using Spread Operator 
+
+const obj1 = {1: "Mayuri" , 2:"barapatre"};
+const obj2 = {3: "Mayuri" , 4:"barapatre"};
+
+const obj3 = {...obj1 , ...obj2};
+console.log(obj3);
+
+// Google this hasOwnProperty --> add atrribute name 
+console.log(obj3.hasOwnProperty(3));
+
+console.log(Object.keys(obj3));
+console.log(Object.values(obj3));
+console.log(Object.entries(obj3));
+
+
+// -----------------------
+
+// const {attributeName} = objectName --- Destructuring the object 
+
+
+// API - earlier was xml , now its json , can be nexted of arrays 
+/* 
+    {
+        "name" : "Mayuri",
+        "age" : 12,
+        "hello": "world"
+    }
+
+    {
+        {}
+        {}
+    }    
+*/
---
+++ 03_Functions/01function.js
@@ -0,0 +1,55 @@
+// code -> package
+
+function sayHello(){
+    console.log("Mayuri U can !!");
+}
+sayHello();
+
+// Function may have parameters and arguments 
+// functions Return value 
+
+/*
+
+- Scope of function 
+undefined is considered as false value -> if(!username) thus use this 
+- when u dont know the number of parameters , use rest operator  (...) also known as spread but based on context
+- bundle mein mil raha hai 
+
+*/
+
+function shoppingCart(item1 , ...cart) {
+    return cart
+}
+
+console.log(shoppingCart("chair", "sunScreen", "Charger"));
+// [ 'sunScreen', 'Charger' ] 
+// cause now item1 will hold "chair"
+
+let anyObject = {
+    name : "sam",
+    age : 12
+}
+function handleObject(anyObject) {
+    console.log(`Hii ${anyObject.name} your age is ${anyObject.age}`);
+    
+}
+// in the same way u can handle the array as well 
+
+handleObject(anyObject)
+
+// ---------------------------------------------
+// Scopes 
+// If already the variables could be used then what was the problem , but scope causes propblem 
+// beacuse then variables become scope independent 
+// let and const are scope dependent {}-> block scope else without that its global scope 
+
+let a = "Global Scope"
+
+if(true) {
+    let a = "Block Scope"
+    console.log(a);  //     1st 
+}
+
+console.log(a);   //    2nd
+
+//Global scopes differ in web browser and the code terminal 
