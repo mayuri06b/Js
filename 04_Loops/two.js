55306d71446ad2a9458f78eb02e38fe89e366357
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-19 21:13:13 +0000

    commited

---
+++ 04_Loops/two.js
@@ -0,0 +1,11 @@
+// FurtHER more on LOOP 
+
+// forEach ke andar tumhe callback Function pass karana hi padega 
+// filter(callback)
+
+const myNums = [1 ,2 ,3 ,4, 8,6 ,67, 7]
+
+// if u open scope then u  must write return else can use () then no return 
+
+// let newNums = myNums.filter((nums)=> {return num > 4})
+
