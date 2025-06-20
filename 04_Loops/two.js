a07e1ddefe7781bd3c0d17e18cd00f5204d6f2e0
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-06-20 07:20:27 +0000

    Basics Done

--- 04_Loops/two.js
+++ 04_Loops/two.js
@@ -3,9 +3,48 @@
 // forEach ke andar tumhe callback Function pass karana hi padega 
 // filter(callback)
 
-const myNums = [1 ,2 ,3 ,4, 8,6 ,67, 7]
+const myNums = [2 , 3 , 5 ,6 ,7 , 8]
 
 // if u open scope then u  must write return else can use () then no return 
 
 // let newNums = myNums.filter((nums)=> {return num > 4})
 
+// Chaining is also a Concept in which u can chain multiple Functions 
+
+// - filter (conditioning )
+// - map changing puropse 
+
+let newChangedArr = myNums.map((num) => num + 10);
+console.log(newChangedArr);
+
+// example of Chaning 
+// 1 st one is passed to the next Chain 
+
+let newArr = myNums.map((num)=> num + 4).filter((num)=> num > 10)
+console.log(newArr);
+
+
+//-----------------------------------Reduce 
+
+myShoppingCart = [
+    {
+        item:"Dress",
+        cost:12000
+    },
+    {
+        item:"Tea Bags",
+        cost:2500
+    },
+    {
+        item:"Shopping Bag",
+        cost:25
+    },
+    {
+        item:"Worlsk",
+        cost:230
+    }
+];
+
+const totalCost = myShoppingCart.reduce((sum , curr)=> (sum+curr.cost) ,0);
+console.log("Total Cost is ");
+console.log(totalCost);
