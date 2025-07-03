2d7b6783ff964679418a114a8027fc89bc694e49
Mayuri Barapatre <mayuribarapatre9@gmail.com>
2025-07-03 11:42:27 +0000

    Completed With The project

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,9 +1,34 @@
+let notesArray = JSON.parse(localStorage.getItem("notes")) || [];
+
+window.onload = function () {
+        notesArray.forEach(note => {
+        createNode(note.color, note.title, note.content , note.date); 
+    });
+
+};
+
+if(notesArray.length >= 1 ){
+    let deleteAll = document.createElement('button');
+    deleteAll.innerHTML = 'Delete All';
+    deleteAll.setAttribute('class' , 'deleteAll-button');
+    document.querySelector('.content').appendChild(deleteAll);
+    deleteAll.addEventListener('click' , ()=>{
+        localStorage.clear();
+        location.reload()
+    })
+}else {
+    let showMessage = document.createElement('p')
+    showMessage.innerHTML = 'No Notes Currently Start creating !!';
+    document.querySelector('.content').appendChild(showMessage);
+}
+
 const colorCodes ={
     red:'#f16f6f',
     orange:'#f1bf6f',
     purple:'#8b6ff1',
     yellow:'#f1d96f'
 }
+
 
 const createButton = document.querySelectorAll('.color-button')
 
@@ -16,34 +41,48 @@ createButton.forEach(button => {
     })
 });
 
-function createNode(color) {
+function createNode(color, titleText = '', contentText = '' , dateValue = '') {
     let div = document.createElement('div');
     div.setAttribute('class', 'note');
+    div.style.backgroundColor = color;
 
     let title = document.createElement('input');
-    title.setAttribute('placeholder' , 'Title')
-    title.setAttribute('class' , 'title-content')
+    title.setAttribute('placeholder', 'Title');
+    title.setAttribute('class', 'title-content');
+    title.value = titleText;
 
     let contentSection = document.createElement('textarea');
     contentSection.setAttribute('placeholder', 'Add content here');
     contentSection.setAttribute('class', 'input-content');
+    contentSection.value = contentText;
 
-    let saveButton = document.createElement('button')
-    saveButton.innerHTML = 'Save'
-    saveButton.setAttribute('class' , 'save-button')
-
-    // let editButton = document.createElement('button')
-    // saveButton.innerHTML = 'Edit'
-    // saveButton.setAttribute('class' , 'edit-button')
-
-    // div.append(editButton)
-
-   
-    div.appendChild(title)
-    div.appendChild(contentSection)
-    div.append(saveButton)
-    div.style.backgroundColor = color;
+    let saveButton = document.createElement('button');
+    saveButton.innerHTML = 'Save';
+    saveButton.setAttribute('class', 'save-button');
+    
+    let date = document.createElement('p')
+    date.innerHTML = new Date().toLocaleDateString();
+    date.style.color = 'white'
+    div.append(date)
+    div.appendChild(title);
+    div.appendChild(contentSection);
+    div.appendChild(saveButton);
 
     document.querySelector('.content').appendChild(div);
-    console.log(div);
+
+    saveButton.addEventListener('click', () => {
+        const noteData = {
+            title: title.value,
+            content: contentSection.value,
+            color: color,
+            dateValue: date.innerHTML
+        };
+        location.reload()
+        displayandSaveNote(noteData);
+    });
+}
+
+function displayandSaveNote(noteData) {
+    notesArray.push(noteData);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
 }
